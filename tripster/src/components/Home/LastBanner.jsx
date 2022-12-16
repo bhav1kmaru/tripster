3f61d47496b837bcbd75a719107f057f7961c96f
bhav1kmaru <bhavik.m01@gmail.com>
2022-12-16 19:06:25 +0530

    changed banner

--- tripster/src/components/Home/LastBanner.jsx
+++ tripster/src/components/Home/LastBanner.jsx
@@ -1,8 +1,10 @@
+import { Box, Button } from '@chakra-ui/react'
 import React from 'react'
+//https://i.ibb.co/Xt4s2Gk/image-35.png
 
 const LastBanner = () => {
   return (
-    <div style={{marginTop:"70px",cursor:"pointer",marginBottom:"150px"}}><img src="https://i.ibb.co/Xt4s2Gk/image-35.png" alt="image-35" border="0" /></div>
+    <Box w="100%" m="auto" mt="100px" mb="200px" height="600px" backgroundImage="url('https://i.ibb.co/Xt4s2Gk/image-35.png')"><Button w='155px' h='50px' variant="solid"  borderRadius="20px" bgColor="black" color="white" _hover={{backgroundColor:"#202020"}} ml="437px" mt="408px">See the winners</Button></Box>
   )
 }
 
